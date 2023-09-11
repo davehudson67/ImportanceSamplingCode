0eef0c9aa90f84425412848430dbef71cb6ea3b3
Hudson <dh526@rstudio03n.isad.isadroot.ex.ac.uk>
2023-09-11 11:12:56 +0100

    Initial commit

---
+++ ToyExample/SimulateData.R
@@ -0,0 +1,23 @@
+## Bayesian model comparison techniques blog
+## load libraries
+library(tidyverse)
+library(nimble)
+library(bayestestR)
+
+## source additional functions
+source("ModelComparison_FUNCTIONS.R")
+
+## set up linear regression data
+# generate independent variable values
+x <- seq(-5, 5, by = 0.1)
+
+# generate true parameter values
+beta_0_true <- 1.2
+beta_1_true <- 0.6
+sigma_true <- 1
+
+# generate dependent variable values
+y <- beta_0_true + beta_1_true * x + rnorm(length(x), mean = 0, sd = sigma_true)
+plot(x, y)
+
+save.image("Sim_Data.RData")
